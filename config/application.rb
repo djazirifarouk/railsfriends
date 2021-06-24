7a215aa7c535eba3e1965c0873bc1ebf2efcfcbd
Farouk Djaziri <faroukjaziri1997@outlook.fr>
2021-06-24 19:30:11 +0100

    fix error

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Friends
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    # config/application.rb
     config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
